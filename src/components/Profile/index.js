9bac5ae0c6de655f5bcb4069c0d390c996b936da
Marcos Corsi <marcosvcorsi@gmail.com>
2020-06-21 21:56:39 -0300

    profile component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Profile = () => {
+  return (
+    <div className="Profile-wrapper">
+      <h1>Marcos Vinicius Corsi</h1>
+      <h2>Engenheiro de Software</h2>
+
+      <p>
+        Membro do core Team da Empresa Teste, escrevo sobre desenvolvimento de
+        software
+      </p>
+    </div>
+  )
+}
+
+export default Profile
