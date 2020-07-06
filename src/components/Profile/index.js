48527def0fa0a0ba621206948b3fb4452acd871d
Marcos Corsi <marcosvcorsi@gmail.com>
2020-07-05 21:09:04 -0300

    Imagens

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,33 +1,36 @@
 import React from "react"
-import { StaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
+
+import Avatar from "../Avatar"
 
 const Profile = () => {
-  return (
-    <StaticQuery
-      query={graphql`
-        query MySiteMetadata {
-          site {
-            siteMetadata {
-              title
-              position
-              description
-            }
+  const {
+    site: {
+      siteMetadata: { title, position, description },
+    },
+  } = useStaticQuery(
+    graphql`
+      query MySiteMetadata {
+        site {
+          siteMetadata {
+            title
+            position
+            description
           }
         }
-      `}
-      render={({
-        site: {
-          siteMetadata: { title, position, description },
-        },
-      }) => (
-        <div className="Profile-wrapper">
-          <h1>{title}</h1>
-          <h2>{position}</h2>
+      }
+    `
+  )
 
-          <p>{description}</p>
-        </div>
-      )}
-    />
+  return (
+    <div className="Profile-wrapper">
+      <Avatar />
+
+      <h1>{title}</h1>
+      <h2>{position}</h2>
+
+      <p>{description}</p>
+    </div>
   )
 }
 
